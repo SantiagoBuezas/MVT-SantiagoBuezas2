1ae7156ba70cfe2cdd016d4fb5d6de88160ff526
Santiago Buezas <sbues2@gmail.com>
2022-10-02 17:18:11 -0300

    Casi terminado con errores

--- ProjectoCoder/ProjectoCoder/settings.py
+++ ProjectoCoder/ProjectoCoder/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'ProjectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            f"{BASE_DIR}/ProjectoCoder/AppCoder/templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
