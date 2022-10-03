5e910e13619d7acf7a314a7024a417a98491be06
Santiago Buezas <sbues2@gmail.com>
2022-10-03 19:17:52 -0300

    Arreglo la fecha y se guarda en la base de datos, entregable terminado

--- ProjectoCoder/AppCoder/views.py
+++ ProjectoCoder/AppCoder/views.py
@@ -5,9 +5,12 @@ from AppCoder.models import Familiar
 
 # Create your views here.
 def mostrar_familiares(request):
-    familiar_1 = Familiar(nombre="Santiago",parentezco="Hijo",edad=31,fecha_de_nacimiento=30/5/1991)
-    familiar_2 = Familiar(nombre="Patricia",parentezco="Madre",edad=52,fecha_de_nacimiento=20/9/1962)
-    familiar_3 = Familiar(nombre="Jorge",parentezco="Padre",edad=65,fecha_de_nacimiento=25/10/1956)
+    familiar_1 = Familiar(nombre="Santiago",parentezco="Hijo",edad=31,fecha_de_nacimiento="1991-5-30")
+    familiar_2 = Familiar(nombre="Patricia",parentezco="Madre",edad=52,fecha_de_nacimiento="1962-5-25")
+    familiar_3 = Familiar(nombre="Jorge",parentezco="Padre",edad=65,fecha_de_nacimiento="1965-7-20")
+    familiar_1.save()
+    familiar_2.save()
+    familiar_3.save()
     contexto = {"familiar_1": familiar_1, "familiar_2":familiar_2,"familiar_3":familiar_3}
     return render(request, "AppCoder/familiares.html",contexto)
 
